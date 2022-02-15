79a9b4e038fc853e1cc3e9d0cefef227e32209c7
ddruganov <ddruganov@bk.ru>
2022-02-15 09:01:34 +0300

    dockerized

--- README.md
+++ README.md
@@ -4,12 +4,9 @@ Console utility to convert cryptopro4 and cryptopro5 certificate (gost-2001, gos
 
 Tested on alpine 3.15 for docker
 
-## prepare
-`$ ./prepare.sh` downloads and installs gost engine for openssl1.1.1
+## how-to
 
-## build
-
-`$ ./build.sh` outputs `cert-convert`
-
-## usage
-`$ cert-convert folder.000 password > certificate.pem`
+1. `docker-compose up`
+2. `docker exec -it get-cpcert.alpine sh`
+3. `./build.sh`
+4. `./build/get-cpcert folder.000 [password] > certificate.pem`
--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-gcc -o cert-convert \
-  -Igost-engine cert-convert.c \
-  -Llibs/openssl-OpenSSL_1_1_1-stable \
-  -lssl \
-  -lcrypto \
-  -Lgost-engine/build \
-  -lgost_core \
-  -L. \
-  -lgost \
-  -lpthread \
-  -ldl \
-  -Xlinker '-rpath=.'
+mkdir -p build
+gcc -o build/get-cpcert \
+    -I/opt/gost-engine src/get-cpcert.c \
+    -Llibs/openssl-OpenSSL_1_1_1-stable \
+    -lssl \
+    -lcrypto \
+    -L/opt/gost-engine/build \
+    -lgost_core \
+    -L. \
+    -lgost \
+    -lpthread \
+    -ldl \
+    -Xlinker '-rpath=.' &>/dev/null
+echo "done"
